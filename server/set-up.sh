2942c3175c14861a0b232ba4c227fdcc32eb5f74
Inhishonor <inhishonor@protonmail.com>
2025-05-10 14:18:02 -0700

    set-up: permissions fixes, and command line flag

--- server/set-up.sh
+++ server/set-up.sh
@@ -1,28 +1,35 @@
 # A bash script used for setting up a server on debian/ubuntu. WIP
+# To run on linux: sudo bash -c 'wget -O - https://raw.githubusercontent.com/Inhishonor/bash-scripts/refs/heads/main/server/set-up.sh | bash'
 # Update the system
-sudo apt update -y
-sudo apt upgrade -y
+echo "Updating System"
+apt update -y
+apt upgrade -y
 
 # Install Tools
-sudo apt install smartmontools -y
-sudo apt install nano -y
-sudo apt install ufw -y
-sudo apt install scp -y
+echo "Installing and Updating Tools"
+apt install smartmontools -y
+apt install nano -y
+apt install ufw -y
 
 #Install OpenVPN client
-echo "Would you like to install and configure an open vpn client? [y/n]"
-read vpnanswer
-if [ "$vpnanswer" = "y" ]; then
-  sudo apt install openvpn -y
-  echo "Please copy the contents of the client.conf file from the open VPN server."
-  sudo nano /etc/openvpn/client.conf 
-  echo "Press enter when you have finished editing the file..."
-  read
-  echo "Next, the script is going to test if the VPN works. The terminal will lock into execution, while you will need to verify the connection at the OpenVPN server. Then return here, and press ctrl + c, to stop the process and this script."
-  sudo openvpn --config /etc/openvpn/client.conf || echo "There was an error, please double check the vpn configuration, and run this script again. Press enter to exit."
-  read
-else
-  echo "That's it!"
-fi
-  
+while getopts ":v" opt; do
+  case $opt in
+    v)
+      echo "Installing OpenVPN client..."
+      apt install -y openvpn
+      echo "Please copy the contents of the client.conf file from the OpenVPN server."
+      nano /etc/openvpn/client.conf 
+      echo "Press enter when you have finished editing the file..."
+      read
+      echo "Next, the script is going to test if the VPN works. The terminal will lock into execution, while you will need to verify the connection at the OpenVPN server. Then return here, and press ctrl + c, to stop the process and this script."
+      openvpn --config /etc/openvpn/client.conf || { echo "There was an error, please double check the VPN configuration, and run this script again. Press enter to exit."; read; }
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+  esac
+done
+
+echo "That's it!"
 
