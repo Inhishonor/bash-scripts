72dc28f6d8388fc9c873acab6f4cdbad82d04ece
Inhishonor <inhishonor@protonmail.com>
2025-06-21 15:15:00 -0700

    emulator: use right directories, and add logging

--- android/emulator.sh
+++ android/emulator.sh
@@ -1,14 +1,21 @@
 # This is a script that builds an image for an emulator on LineageOS.
 echo "This script builds and starts an eng emulator for x86_64"
 
+# Navigate back to Lineage directory
+cd android/lineage
+
 # Setup build environment
 source build/envsetup.sh
+
+# Make sure we are still in right directory
+croot
 
 # Set Target
 breakfast sdk_phone_x86_64 eng
 
 # Build Image
 mka
+echo "Image built! Starting emulator..."
 
 # Start Emulator
 emulator
