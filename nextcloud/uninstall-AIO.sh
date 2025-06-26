3dce15446920defe1c62e0924e7ffa197e4a520d
Inhishonor <inhishonor@protonmail.com>
2025-06-26 16:02:09 -0700

    nextcloud: Fix syntax errors and better error handling

--- nextcloud/uninstall-AIO.sh
+++ nextcloud/uninstall-AIO.sh
@@ -2,12 +2,13 @@
 # TODO: Add better error handling.
 # See this url https://github.com/nextcloud/all-in-one#how-to-properly-reset-the-instance
 #!/bin/bash
+set -e
 echo -e "\033[31mWarning: This script is not complete yet. Do not use it in production!\033[0m"
 exec < /dev/tty
 echo "Hello, this script reset's a Nextcloud AIO instance. Would you like to continue? [y/n]"
 read firstanswer
 exec <&-
-if [ "$firstanswer" = "y"]; then
+if [ "$firstanswer" = "y" ]; then
   sudo docker stop nextcloud-aio-mastercontainer > /dev/null 2>&1
   echo "Removed Nextcloud-AIO-Mastercontainer"
   
@@ -25,7 +26,7 @@ if [ "$firstanswer" = "y"]; then
   echo "Would you like to remove these volumes? [y/n]"
   read volumeanswer
   exec <&-
-  if [ "$volumeanswer" = "y"]; then
+  if [ "$volumeanswer" = "y" ]; then
     echo "Removing all dangling volumes"
     sudo docker volume prune --filter all=1 > /dev/null 2>&1
     echo "All dangling volumes removed"
@@ -35,20 +36,22 @@ if [ "$firstanswer" = "y"]; then
     echo "Would you like to remove all docker images? [y/n]"
     read dockeranswer
     exec <&-
-    if [ "$dockeranswer" = "y"]; then
+    if [ "$dockeranswer" = "y" ]; then
       sudo docker image prune -a
     else
       echo "All right then."
+    fi
   else
     echo "You will have to remove all volumes starting with nextcloud-aio to reset the instance. You can do that manually with sudo docker volume rm <volume_name>."
   fi
-else
+
   exec < /dev/tty
   echo "Would you like to remove all docker images? [y/n]"
   read dockeranswer
   exec <&-
-  if [ "$dockeranswer" = "y"]; then
+  if [ "$dockeranswer" = "y" ]; then
     sudo docker image prune -a
   else
     echo "Never mind then"
+  fi
 fi
